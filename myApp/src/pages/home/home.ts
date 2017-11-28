5b35384c2a4589e5e69bbf24895e178df3c39797
thornax <thornax007>
2017-11-28 16:38:04 +0100

    update app

--- myApp/src/pages/home/home.ts
+++ myApp/src/pages/home/home.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+//import { List } from 'ionic-angular';
+//import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+// @ViewChild(List) list: List;
 
   constructor(public navCtrl: NavController) {
 
+
   }
 
+  openModal() {
+   console.log('knoptest');
+   //this.list.enableSlidingItems(true);
+ }
+
+  //openModal() {
+
+  //}
 }
