5ec6b9366d0ef53e178f0a601e1b12c4707de43c
thornax <thornax007>
2017-12-08 14:32:47 +0100

    buttonslide

--- myApp/src/pages/slides/slides.ts
+++ myApp/src/pages/slides/slides.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { ModalController } from 'ionic-angular';
-import { Camera, CameraPreviewRect, CameraPreview, CameraPreviewPictureOptions, CameraPreviewOptions, CameraPreviewDimensions } from '@ionic-native/camera';
+import { NavController } from 'ionic-angular';
+import { Slides } from 'ionic-angular';
+import { Component,ViewChild } from '@angular/core';
+//import { Camera, CameraPreviewRect, CameraPreview, CameraPreviewPictureOptions, CameraPreviewOptions, CameraPreviewDimensions } from '@ionic-native/camera';
 //import { CameraPreview, CameraPreviewRect } from 'ionic-native';
 
 
@@ -13,14 +16,19 @@ import { Camera, CameraPreviewRect, CameraPreview, CameraPreviewPictureOptions, 
 
 
 export class SlidesPage {
+  @ViewChild(Slides) slides: Slides;
 
-  constructor(public modalCtrl: ModalController/*, public cameraPreview: CameraPreview*/) {
+  constructor(public modalCtrl: ModalController, public navCtrl: NavController/*, public cameraPreview: CameraPreview*/) {
   }
+gotoNextSlide() { this.slides.slideNext(); }
+//gotoUser(){
 
+  //this.navCtrl.push(toUser);
+//  }
 
-  startCamera() {
-    //console.log(startcamera)
-      CameraPreview.startCamera({x: 40, y: 100, width: 80, height: 220, toBack: false, previewDrag: true});
-    }
+  // startCamera() {
+  //   //console.log(startcamera)
+  //     CameraPreview.startCamera({x: 40, y: 100, width: 80, height: 220, toBack: false, previewDrag: true});
+  //   }
 
 }
